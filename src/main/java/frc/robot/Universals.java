ddee474b6945b19e4f9dff53121cfe2947d7befb
FRC5464 <bluejacketprogramming@gmail.com>
2026-01-15 17:05:14 -0600

    pathplannerlib installed

---
+++ src/main/java/frc/robot/Universals.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+public class Universals {
+    
+
+    public static boolean zeroGyro = false;
+    public static boolean brakeMode = false;
+    public static double wait = 0;
+    public static boolean zoomMode = false;
+
+
+}
