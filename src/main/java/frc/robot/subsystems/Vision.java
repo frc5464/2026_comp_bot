64eb477aacce4614b6e681309bcdb55c685a7282
LCMadsen <bluejacketprogramming@gmail.com>
2026-01-15 16:55:00 -0600

    Update Vision.java
    AprilTag readouts with yaw, pitch, area, and skew outputs.

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,61 +1,49 @@
 package frc.robot.subsystems;
-import java.lang.annotation.Target;
 import java.util.List;
 
 //import javax.xml.crypto.dsig.Transform;
 
 import org.photonvision.PhotonCamera;
+import org.photonvision.targeting.PhotonPipelineResult;
 //import org.photonvision.targeting.PhotonPipelineResult;
 //import org.photonvision.targeting.PhotonTrackedTarget;
 import org.photonvision.targeting.PhotonTrackedTarget;
-import org.photonvision.targeting.TargetCorner;
-
-import edu.wpi.first.math.geometry.Transform3d;
 
 public class Vision {
-    
- /* public static void main(String[] args) {
-    final int UPDATES_PER_SECOND = 2400 * 1000;
-    PhotonCamera camera = new PhotonCamera("photonvision");
-    boolean VisionLoop = true;
-    List<TargetCorner> VisCorners = getCorners(camera);
-    final long calculatedUPS = (long) 1/UPDATES_PER_SECOND;
-    while(VisionLoop){
-        try {
-            Thread.sleep(calculatedUPS);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
+
+    PhotonCamera camera = new PhotonCamera("null");
+    private List<PhotonPipelineResult> results = camera.getAllUnreadResults();
+    private PhotonPipelineResult result = results.get(results.size()-1);
+
+    private void visionUpdateLoop(){
+        results=camera.getAllUnreadResults();
+        result = results.get(results.size()-1);
 
     }
-
-  } */
-    static Transform3d getPose(PhotonCamera targetCamera){
-        var result = targetCamera.getLatestResult();
-        PhotonTrackedTarget target = result.getBestTarget();
-        return target.getBestCameraToTarget();}
+    //private PhotonCamera camera = new PhotonCamera("default");
     
-        static List<TargetCorner> getCorners(PhotonCamera targetCamera){
-        var result = targetCamera.getLatestResult();
-        PhotonTrackedTarget target = result.getBestTarget();
-        return target.getDetectedCorners();}
-    
-    static double getDoubleValue(String targetString, PhotonCamera targetCamera){
-        var result = targetCamera.getLatestResult();
-        PhotonTrackedTarget target = result.getBestTarget();
-        switch (targetString) {
-            case "yaw":
-                return target.getYaw();
-            case "pitch":
-                return target.getPitch();
-            case "area":
-                return target.getArea();
-            case "skew":
-                return target.getSkew();
-            default:
-                return -256;
+    public double getTargetInfoDouble (int fidicalID, String targetField) {
+        visionUpdateLoop();
+        if (result.hasTargets()){
+            for (PhotonTrackedTarget i : result.getTargets()) {
+                if (i.getFiducialId() == fidicalID){
+                    switch (targetField) {
+                        case "yaw":
+                            return i.getYaw();
+                        case "pitch":
+                            return i.getPitch();
+                        case "area":
+                            return i.getArea();
+                        case "skew":
+                            return i.getSkew();
+                        default:
+                            continue;
+                    }
+                }
+            }
         }
+        // return zero if no AprilTag found
+        return (double) 0;
     }
-  
+ 
 }
