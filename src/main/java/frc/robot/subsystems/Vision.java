ac0b875476e362277ee6e1584ba190356c944cd5
LCMadsen <bluejacketprogramming@gmail.com>
2026-01-17 14:29:40 -0600

    Removed unnecessary TODO statement

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -78,7 +78,6 @@ public class Vision {
             return new AprilTagFieldLayout(aprilDataPath);
         } 
         catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
         return null;
