f1403961bc5a409db54c586eb8cbec6608fc6e3e
LCMadsen <bluejacketprogramming@gmail.com>
2026-01-17 13:39:07 -0600

    AprilTag based robotPose function
    added super constructor for AprilTagFieldLayout

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -2,6 +2,7 @@ package frc.robot.subsystems;
 import java.io.IOException;
 import java.util.List;
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 import org.photonvision.targeting.TargetCorner;
@@ -17,13 +18,16 @@ import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+
 import org.photonvision.simulation.VisionSystemSim; 
 
 public class Vision {
     // these values should be user-configured as needed
     private PhotonCamera camera = new PhotonCamera("null");
-    private String aprilTagPath = "april_tag_layouts\\2026-rebuilt-welded.json";
-    // these values pull from the aformentioned ones and can be effectively static
+    private final String aprilDataPath = "src\\main\\java\\frc\\robot\\subsystems\\april_tag_layouts\\2026-rebuilt-welded.json";
+    AprilTagFieldLayout aprilField = constructAprilField(aprilDataPath);
+    // these values pull from the aformentioned ones and can stay the same
     private List<PhotonPipelineResult> results = camera.getAllUnreadResults();
     private PhotonPipelineResult result = results.get(results.size()-1);
     private VisionSystemSim visionLayout = new VisionSystemSim("primary");
@@ -37,7 +41,6 @@ public class Vision {
     private Matrix<N3, N1> swerveStdDev;
     private Matrix<N3, N1> swerveVisMeasurementStdDev;
 
-
     private final SwerveDrivePoseEstimator mainPoseEst = new SwerveDrivePoseEstimator(
         swerveDriveKin,
         swerveGyroAngle,
@@ -48,17 +51,21 @@ public class Vision {
         
     );
     
-
+    // apriltag super constructor with t/c
+    private AprilTagFieldLayout constructAprilField(String jsonPath){
+        try {
+            return new AprilTagFieldLayout(aprilDataPath);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
+    }
     //# of times loopthrough, used for variable declaration
     private int loopCount = 0;
     private void visionUpdateLoop(){
         if (loopCount == 0){
-            // try/catch incase of invalid path
-            try {
-                visionLayout.addAprilTags(new AprilTagFieldLayout(aprilTagPath));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            visionLayout.addAprilTags(aprilField);
         }
         loopCount++;
         // re-read from camera
@@ -113,5 +120,13 @@ public class Vision {
         return (null);
     }
 
+    public Pose3d robotPose (){
+        for (PhotonTrackedTarget i : result.getTargets()) {
+            if (aprilField.getTagPose(i.getFiducialId()).isPresent()) {
+                return PhotonUtils.estimateFieldToRobotAprilTag(i.getBestCameraToTarget(),aprilField.getTagPose(i.getFiducialId()).get(),getTargetInfoPose(i.getFiducialId()));
+            }
+        }
+        return null;
+    }
  
 }
