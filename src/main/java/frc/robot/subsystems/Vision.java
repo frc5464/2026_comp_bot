f7a2d3d0df633bebd2dfccba6ab1187d43623137
LCMadsen <bluejacketprogramming@gmail.com>
2026-01-17 11:11:02 -0600

    Added AprilTag Layouts
    started work on location estimation

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -1,47 +1,45 @@
 package frc.robot.subsystems;
-import java.lang.ref.Cleaner;
+import java.io.IOException;
 import java.util.List;
-
-
 import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
-
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
-import edu.wpi.first.math.estimator.DifferentialDrivePoseEstimator;
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 
-import org.photonvision.PhotonUtils;
 import org.photonvision.simulation.VisionSystemSim;
 public class Vision {
-    
+    // these values should be user-configured as needed
     private PhotonCamera camera = new PhotonCamera("null");
+    private String aprilTagPath = "april_tag_layouts\\2026-rebuilt-welded.json";
+    
+    // these values pull from the aformentioned ones and can be effectively static
     private List<PhotonPipelineResult> results = camera.getAllUnreadResults();
     private PhotonPipelineResult result = results.get(results.size()-1);
     private VisionSystemSim visionLayout = new VisionSystemSim("primary");
+    private SwerveDrivePoseEstimator swerveDriveMainPose;
     
-    //private final DifferentialDrivePoseEstimator robotPose = 
-    //    new DifferentialDrivePoseEstimator(
-    //        
-    //    );
 
     //# of times loopthrough, used for variable declaration
     private int loopCount = 0;
     private void visionUpdateLoop(){
+        swerveDriveMainPose.update(null, null);
         if (loopCount == 0){
-            visionLayout.addAprilTags(null);
-            //setup AprilTag layout
+            // try/catch incase of invalid path
+            try {
+                visionLayout.addAprilTags(new AprilTagFieldLayout(aprilTagPath));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
         loopCount++;
-        
+
         // re-read from camera
         results=camera.getAllUnreadResults();
         result = results.get(results.size()-1);
 
     }
-    
+    // internal function used for getting double values, requires fiducialID and what value is needed (yaw, pitch, area, etc etc)
     public double getTargetInfoDouble (int fiducialID, String targetField) {
         visionUpdateLoop();
         if (result.hasTargets()){
