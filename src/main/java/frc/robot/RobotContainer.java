f6cd61b7ae19bf6327456121c97d7285cca0ecf9
FRC5464 <bluejacketprogramming@gmail.com>
2026-01-20 16:26:36 -0600

    smooth driving

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,30 +4,53 @@
 
 package frc.robot;
 
-import static edu.wpi.first.units.Units.*;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.pathplanner.lib.auto.AutoBuilder;
+// import com.pathplanner.lib.auto.AutoBuilder;
+// import com.pathplanner.lib.commands.PathPlannerAuto;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
-import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
-
+// import frc.robot.Commands.IntakeCommand;
+import frc.robot.Commands.ZeroGyroCommand;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
+// import frc.robot.subsystems.IntakeSubsystem;
 
 public class RobotContainer {
-    private double MaxSpeed = 0.1 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
+    Universals universals = new Universals();
+    private double MaxSpeed = 0.25 * TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
     private double MaxAngularRate = RotationsPerSecond.of(0.25).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+    // private SubsystemManager subsystemManager;
+    // /**
+    //  * Connects this to joysticks
+    //  * 
+    //  * 
+    //  * @param subsystemManager
+    //  */
+    // public static void create(SubsystemManager subsystemManager){
+       // final CommandSwerveDrivetrain drivesubsystem = subsystemManager.getCommandSwerveDrivetrain();
+        //final IntakeSubsystem intake = subsystemManager.getIntakeSubsystem();
+    
+    // }
 
+
+    // private final SendableChooser<Command> autoChooser;
     
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
-            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.2) // Add a 10% deadband
             .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
     private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
     private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
@@ -35,21 +58,44 @@ public class RobotContainer {
     private final Telemetry logger = new Telemetry(MaxSpeed);
 
     private final CommandXboxController joystick = new CommandXboxController(0);
+    // private final CommandXboxController joystick2 = new CommandXboxController(1);
+    
 
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
 
     public RobotContainer() {
+        // autoChooser = AutoBuilder.buildAutoChooser();
+
         configureBindings();
+
+        // boolean isCompetition = true;
+
+        // Build an auto chooser. This will use Commands.none() as the default option.
+        // As an example, this will only show autos that start with "comp" while at
+        // competition as defined by the programmer
+        // autoChooser = AutoBuilder.buildAutoChooser();
+        // (stream) -> isCompetition
+            // ? stream.filter(auto -> auto.getName().startsWith("comp")) : stream); 
+        // SmartDashboard.putData("Auto Mode", autoChooser);
+
+
+        //controller deadband
+        double driveX = joystick.getRawAxis(1);
+        double driveY = joystick.getRawAxis(0);
+        double driveRot = -joystick.getRawAxis(4);
+        if(Math.abs(driveX) < 0.1){ driveX = 0;}
+        if(Math.abs(driveY) < 0.1){ driveY = 0;}
+        if(Math.abs(driveRot) < 0.1){ driveRot = 0;}
     }
 
-    private void configureBindings() {
+    public void configureBindings() {
         // Note that X is defined as forward according to WPILib convention,
         // and Y is defined as to the left according to WPILib convention.
         drivetrain.setDefaultCommand(
             // Drivetrain will execute this command periodically
             drivetrain.applyRequest(() ->
-                drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
-                    .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+                drive.withVelocityX(joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
+                    .withVelocityY(joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
                     .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
             )
         );
@@ -61,7 +107,10 @@ public class RobotContainer {
             drivetrain.applyRequest(() -> idle).ignoringDisable(true)
         );
 
+        //Brake Mode
         joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+
+        //???? Mode
         joystick.b().whileTrue(drivetrain.applyRequest(() ->
             point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
         ));
@@ -73,15 +122,26 @@ public class RobotContainer {
         joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
         joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
-        // Reset the field-centric heading on left bumper press.
-        joystick.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
+        
+
+        //joystick.leftTrigger(0.1).whileTrue(new IntakeCommand(intake, true));
+        
+        // Zero Gyro              //Reset the field-centric heading on left bumper press.
+        joystick.back().onTrue(new ZeroGyroCommand(drivetrain));
+        // joystick.back().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
+
+        //joystick2.leftBumper().whileTrue(new IntakeCommand(intake, false));
+
+
 
         drivetrain.registerTelemetry(logger::telemeterize);
         
     }
 
     public Command getAutonomousCommand() {
-        // Simple drive forward auton
+        // return autoChooser.getSelected();
+
+        // // Simple driv\e forward auton
         final var idle = new SwerveRequest.Idle();
         return Commands.sequence(
             // Reset our field centric heading to match the robot
@@ -97,5 +157,6 @@ public class RobotContainer {
             // Finally idle for the rest of auton
             drivetrain.applyRequest(() -> idle)
         );
+
     }
 }
--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -24,8 +24,8 @@ public class TunerConstants {
     // The steer motor uses any SwerveModule.SteerRequestType control request with the
     // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
     private static final Slot0Configs steerGains = new Slot0Configs()
-        .withKP(500).withKI(60).withKD(20)
-        .withKS(0.1).withKV(1.40).withKA(0)
+        .withKP(20).withKI(0).withKD(.1)
+        .withKS(0.1).withKV(0).withKA(0)
         .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
     // When using closed-loop control, the drive motor uses the control
     // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
