48d3cb17450c666208ded32510f0c44f6b4274b0
FRC5464 <bluejacketprogramming@gmail.com>
2026-02-04 09:31:45 -0600

    Intake lifting PID stuff

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -3,11 +3,16 @@ package frc.robot.subsystems;
 import com.pathplanner.lib.config.PIDConstants;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.PersistMode;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.FeedbackSensor;
+import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
-
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -15,40 +20,51 @@ import frc.robot.Universals;
 
 public class IntakeSubsystem extends SubsystemBase{
 
-    
-    public IntakeSubsystem(){}
-
-
     private final SparkMax leftRotator = new SparkMax(50, MotorType.kBrushless);
     private final SparkMax rightRotator = new SparkMax(51, MotorType.kBrushless);
     
     private final SparkMax intakeRod = new SparkMax(52, MotorType.kBrushless);
 
-    public RelativeEncoder leftEncoder;
-    public RelativeEncoder rightEncoder;
+    SparkMaxConfig sparkMaxConfig1;
+    double kP = 0;
+    double kI = 0;
+    double kD = 0;
+    double kIz = 0;
+    double kFF = 0;
+    double extMaxOutput = 0;
+    double extMinOutput = 0;
+    RelativeEncoder leftEncoder;
+    RelativeEncoder rightEncoder;
+    public double leftEncoderPos;
+    public double rightEncoderPos;
+    public double counts;
+    public int level = 0;
 
-  private static final boolean ENABLED = true;
+    public double targetPosition = 0.5;
 
-  SparkMaxConfig sparkMaxConfig3 = new SparkMaxConfig();
-  // SparkClosedLoopController loopController = climb1.getClosedLoopController();
-  SparkClosedLoopController leftPID = leftRotator.getClosedLoopController();
-  SparkClosedLoopController rightPID = rightRotator.getClosedLoopController();
-  double kP = 0;
-  double kI = 0;
-  double kD = 0;
-  double kIz = 0;
-  double kFF = 0;
-  double extMaxOutput = 0;
-  double extMinOutput = 0;
+    private SparkMaxConfig leftRotConfig = new SparkMaxConfig();
+    private SparkMaxConfig rightRotConfig = new SparkMaxConfig();
+    private SparkClosedLoopController leftController = leftRotator.getClosedLoopController();
+    private SparkClosedLoopController rightController = rightRotator.getClosedLoopController();
+    private RelativeEncoder leftrelativeEncoder;
+    private RelativeEncoder rightRelativeEncoder;
+    private double maxPower = 0.3;
 
-    // private final Encoder encoder = new Encoder(null, null);
+    public IntakeSubsystem(){
+        leftController = leftRotator.getClosedLoopController();
+        rightController = rightRotator.getClosedLoopController();
 
-    // private final PIDController intakeFeedback = new PIDController(0, 0, 0);
-    // DigitalInput searchFuel = new DigitalInput(0);
-    // private static final boolean ENABLED = true;
-    // public boolean fuelnotdetected = false;
+        leftrelativeEncoder = leftRotator.getEncoder();
+        rightRelativeEncoder = rightRotator.getEncoder();
+
+        leftRotConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder).p(0.3).i(0).d(0).outputRange(-0.3, 0.3);
+
+        leftRotator.configure(leftRotConfig, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+    }
 
     public void periodic(){
+        SmartDashboard.putNumber("leftIntakeEncoder", leftrelativeEncoder.getPosition());
+        leftController.setReference(targetPosition, ControlType.kPosition, ClosedLoopSlot.kSlot0);
         // SmartDashboard.putBoolean("feeding", Universals.feeding);
         // SmartDashboard.putBoolean("Intaking", Universals.intaking);
     }
@@ -61,6 +77,10 @@ public class IntakeSubsystem extends SubsystemBase{
     public void ManualLowerIntake(){
         leftRotator.set(-1);
         // rightRotator.set(1);
+    }
+
+    public void intakeUp(){
+
     }
 
     public void stopElevate(){
