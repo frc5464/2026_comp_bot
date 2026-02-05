30ae64507648ca6df5288f6c88ec740a45da40c6
FRC5464 <bluejacketprogramming@gmail.com>
2026-02-04 18:23:44 -0600

    shoot elevate commands made

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
@@ -9,6 +10,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class TurretSubsystem extends SubsystemBase{
     
     private final SparkMax turret = new SparkMax(66, MotorType.kBrushless);
+
+    public SparkClosedLoopController turretController = turret.getClosedLoopController();
 
     public void clockwise(){
         turret.set(1);
