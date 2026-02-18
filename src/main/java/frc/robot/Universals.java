6e4e575a57702b69b2f6d35860e3d3615187c576
FRC5464 <bluejacketprogramming@gmail.com>
2026-02-18 17:39:43 -0600

    start of on the fly pathing

--- src/main/java/frc/robot/Universals.java
+++ src/main/java/frc/robot/Universals.java
@@ -1,5 +1,8 @@
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
 public class Universals {
     
 
@@ -9,5 +12,36 @@ public class Universals {
     public static boolean zoomMode = false;
     public static boolean slowMode = false;
 
+    public static boolean intaking = false;
+    public static boolean feeding = false;
+    // public static boolean intakeReverse = false;
+    
+    public static boolean climbUp = false;
+    public static boolean climbDown = false;
+    public static boolean autoClimbUp = false;
+    public static boolean autoClimbDown = false;
 
+    public static boolean climbOverride = false;
+
+    // public static String LEDselected = "White";
+
+    public static boolean snapAprilHub = false;
+
+    public static boolean closeToHub = false;
+
+    public static boolean fuelHoming = false;
+    public static boolean fuelDetected = false;
+
+    public static boolean aprilTagsAreDetected = false;
+    
+    public static boolean dummyIntake = false;
+
+    public static boolean homingPathToFuel = false;
+
+    public static boolean translateToNote = true;
+
+    // public static Pose2d RSL1 = new Pose2d(4.625, 2.5, Rotation2d.fromDegrees(180));
+    private Universals(){
+
+    }  
 }
