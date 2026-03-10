0eae3c1770168e65c18dab557fe385f73fa3c6ed
FRC5464 <bluejacketprogramming@gmail.com>
2026-03-10 15:41:49 -0500

    print calcturretpos

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -166,5 +166,6 @@ public class TurretSubsystem extends SubsystemBase{
         // }
 
         SmartDashboard.putNumber("angletoHub", angletoHub);
+        SmartDashboard.putNumber("calcTurretPos", calculatedPosition);
     }
 }
