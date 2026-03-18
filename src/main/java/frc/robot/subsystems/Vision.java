2c6fef8226df254aab86c97195289ced63cd237d
FRC5464 <94130236+frc5464@users.noreply.github.com>
2026-03-18 11:56:44 -0500

    started work on rotation to target

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -7,31 +7,36 @@ import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Telemetry;
 
 public class Vision extends SubsystemBase {
 
     public PhotonCamera[] camera = {
-            new PhotonCamera("driver")
+            new PhotonCamera("driver") /*driver cam wont actually be used for vision due to variable position */
     };
 
-    public List<PhotonPipelineResult> newest_i(String targ) {
+    public List<PhotonPipelineResult> newest_scan(String targ) {
         // for each camera, check if it matches target and return results if so
+        List<PhotonPipelineResult> compiled_targets = new ArrayList<>();
         for (int i = 0; i < camera.length; i++) {
             if (camera[i].getName() == targ) {
                 return camera[i].getAllUnreadResults();
             }
-
+            if (targ == ""){
+                compiled_targets.addAll(camera[i].getAllUnreadResults());
+            }
+        }
+        if (targ == ""){
+            // used if you want all results instead of just those of a specfic camera
+            return compiled_targets;
         }
         System.out.println("Camera not found. This is bad !! check for typos");
         return null;
     }
 
-    public List<PhotonTrackedTarget> targets_i(String targ) {
+    public List<PhotonTrackedTarget> targets_all(String targ) {
         // source pipeline results
-        List<PhotonPipelineResult> origin = newest_i(targ);
+        List<PhotonPipelineResult> origin = newest_scan(targ);
 
         // result compiled list
         List<PhotonTrackedTarget> RCL = new ArrayList<>();
@@ -59,4 +64,13 @@ public class Vision extends SubsystemBase {
 
     }
 
+    public void rotate_to_tag(int targetAprilTag){
+        List<PhotonTrackedTarget> all_detected = targets_all(""); /*pass empty to use all cameras */
+
+        for (PhotonTrackedTarget at : all_detected) {
+            if (at.getFiducialId() == targetAprilTag){
+                //TODO: implement rotation
+            }
+        }
+    }
 }
