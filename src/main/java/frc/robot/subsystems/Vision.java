1ae79569b706a7eda82fd8192755368d2b8d0254
FRC5464 <94130236+frc5464@users.noreply.github.com>
2026-03-18 16:17:10 -0500

    periodic integration

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -7,6 +7,7 @@ import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonPipelineResult;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Vision extends SubsystemBase {
@@ -15,17 +16,27 @@ public class Vision extends SubsystemBase {
             new PhotonCamera("driver") /*driver cam wont actually be used for vision due to variable position */
     };
 
+    @Override
+    public void periodic(){
+        newest_scan("allVision");
+    }
+
     public List<PhotonPipelineResult> newest_scan(String targ) {
         // for each camera, check if it matches target and return results if so
         List<PhotonPipelineResult> compiled_targets = new ArrayList<>();
         for (int i = 0; i < camera.length; i++) {
             if (camera[i].getName() == targ) {
+                SmartDashboard.putString("AT found",camera[i].getAllUnreadResults().toString());
                 return camera[i].getAllUnreadResults();
             }
-            if (targ == ""){
+            if (targ == "allVision" && camera[i].getName() != "driver"){
                 compiled_targets.addAll(camera[i].getAllUnreadResults());
             }
+            SmartDashboard.putString("AT found",camera[i].getAllUnreadResults().toString());
         }
+
+            
+
         if (targ == ""){
             // used if you want all results instead of just those of a specfic camera
             return compiled_targets;
@@ -37,10 +48,10 @@ public class Vision extends SubsystemBase {
     public List<PhotonTrackedTarget> targets_all(String targ) {
         // source pipeline results
         List<PhotonPipelineResult> origin = newest_scan(targ);
-
+        
         // result compiled list
         List<PhotonTrackedTarget> RCL = new ArrayList<>();
-
+        SmartDashboard.putString("AT targets",RCL.toString());
         boolean has_target = false;
         for (int i = 0; i < origin.size(); i++) {
             if (origin.get(i).hasTargets()) {
@@ -66,10 +77,14 @@ public class Vision extends SubsystemBase {
 
     public void rotate_to_tag(int targetAprilTag){
         List<PhotonTrackedTarget> all_detected = targets_all(""); /*pass empty to use all cameras */
-
+        
         for (PhotonTrackedTarget at : all_detected) {
             if (at.getFiducialId() == targetAprilTag){
                 //TODO: implement rotation
+                //  take corners of given at + area + pitch/yaw/rotation
+                //  rotate shooter (after reset) until match?
+                //  check in with gabe/michael on how the shooter works
+                //  actually get to test the code ???
             }
         }
     }
