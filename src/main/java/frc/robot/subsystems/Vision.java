36054d3181fa16c3dc6308db1088e4de008acc0e
FRC5464 <94130236+frc5464@users.noreply.github.com>
2026-03-24 11:27:47 -0500

    vision rework (untested, commented out)
    shouldnt change main code stability since nothing's running on periodic. implemented a lookup table for camera offsets

--- src/main/java/frc/robot/subsystems/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision.java
@@ -5,95 +5,101 @@ import java.util.List;
 
 import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonPipelineResult;
-import org.photonvision.targeting.PhotonTrackedTarget;
 
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Vision extends SubsystemBase {
+    
+    private class lookupTable {
+        // functions as a dictionary,
+        // where the index of a key is used as the index of a result
+        // interact with using functions, not directly
+        // key/result mismatch may otherwise occur
 
-    public PhotonCamera[] camera = {
-            new PhotonCamera("driver") /*driver cam wont actually be used for vision due to variable position */
-    };
+        List<String> keys = new ArrayList<>();
+        List<List<PhotonPipelineResult>> results = new ArrayList<>();
 
-    // @Override
-    public void periodic(){
-        //this is bad and should be removed most likely
-        //the List<PhotonPipelineResult> isnt used for anything
-        //and this doesnt need to be run periodicly
-        //right now at least
-        //eventually this might be a good idea if it's referenced by more then one other function at a given moment
-        newest_scan("allVision");
-    }
-
-    public List<PhotonPipelineResult> newest_scan(String targ) {
-        // for each camera, check if it matches target and return results if so
-        List<PhotonPipelineResult> compiled_targets = new ArrayList<>();
-        for (int i = 0; i < camera.length; i++) {
-            if (camera[i].getName() == targ) {
-                SmartDashboard.putString("AT found","OC/"+camera[i].getAllUnreadResults().toString());
-                return camera[i].getAllUnreadResults();
-            }
-            if (targ == "allVision" && camera[i].getName() != "driver"){
-                compiled_targets.addAll(camera[i].getAllUnreadResults());
-            }
-            SmartDashboard.putString("AT found","AV/"+camera[i].getAllUnreadResults().toString());
-        
-        
+        public int addItem(String Key, List<PhotonPipelineResult> value){
             
+            if (!keys.contains(Key)){
+                keys.add(Key);
+                results.add(value);
+            } else {
+                results.set(keys.indexOf(Key),value);
+            }
+            
+            return keys.indexOf(Key);
         }
 
-
-        if (targ == "allVision"){
-            // used if you want all results instead of just those of a specfic camera
-            return compiled_targets;
+        public List<PhotonPipelineResult> retrieveItem(String Key){
+            return results.get(keys.indexOf(Key));
         }
-        System.out.println("Camera not found. This is bad !! check for typos");
-        return null;
+
+        public void popItem(String Key){
+            results.remove(keys.indexOf(Key));
+            keys.remove(Key);
+        }
+        public void clear(){
+            results.clear();
+            keys.clear();
+        }
+    }
+    
+    private PhotonCamera[] cameras = {
+        new PhotonCamera("ribombee"), /* are these camera names allowed? like frc-wise */
+        new PhotonCamera("combee"),
+        new PhotonCamera("beedril"),
+        new PhotonCamera("vespiquen")
+    };
+    private lookupTable cameraTable = new lookupTable();
+
+    private Transform3d turretOffset = new Transform3d();
+
+    public void periodic(){
+        // dont uncomment and push to main till stable
+        //VisionLoop();
     }
 
-    public List<PhotonTrackedTarget> targets_all(String targ) {
-        // source pipeline results
-        List<PhotonPipelineResult> origin = newest_scan(targ);
-        
-        // result compiled list
-        List<PhotonTrackedTarget> RCL = new ArrayList<>();
-        SmartDashboard.putString("AT targets",RCL.toString());
-        boolean has_target = false;
-        for (int i = 0; i < origin.size(); i++) {
-            if (origin.get(i).hasTargets()) {
-                has_target = true;
-                break;
-            }
-        }
-        if (!has_target) {
-            System.out.println("Not targetful, exiting iteration");
-            return null;
-        }
-        for (int i = 0; i < origin.size(); i++) {
-            if (origin.get(i).hasTargets()) {
-                RCL.addAll(origin.get(i).getTargets());
-            }
-        }
-        if (RCL.isEmpty()) {
-            System.out.println("warning: empty outs");
-        }
-        return RCL;
 
-    }
 
-    public void rotate_to_tag(int targetAprilTag){
-        List<PhotonTrackedTarget> all_detected = targets_all("allVision"); /*pass empty to use all cameras */
+
+    public void VisionLoop(){
+
+        cameraTable.clear(); /* clear table before adding items again */
         
-        for (PhotonTrackedTarget at : all_detected) {
-            if (at.getFiducialId() == targetAprilTag){
-                SmartDashboard.putBoolean("winner", true);
-                //TODO: implement rotation
-                //  take corners of given at + area + pitch/yaw/rotation
-                //  rotate shooter (after reset) until match?
-                //  check in with gabe/michael on how the shooter works
-                //  actually get to test the code ???
+        for (PhotonCamera c : cameras) {
+
+            // set the relative turret offset for a given camera
+            // default value also included
+            // TODO: replace with actual offsets.
+            switch (c.getName()) {
+                case "ribombee":
+                    turretOffset = new Transform3d();
+                    break;
+                
+                case "combee":
+                    turretOffset = new Transform3d();
+                    break;
+
+                case "beedril":
+                    turretOffset = new Transform3d();
+                    break;
+                
+                case "vespiquen":
+                    turretOffset = new Transform3d();
+                    break;
+                
+                default:
+                    turretOffset = new Transform3d();
+                    break;
             }
+            cameraTable.addItem(c.getName(),c.getAllUnreadResults());
+        }
+
+        for (String key : cameraTable.keys) {
+            SmartDashboard.putString(key,cameraTable.retrieveItem(key).toString());
         }
     }
 }
