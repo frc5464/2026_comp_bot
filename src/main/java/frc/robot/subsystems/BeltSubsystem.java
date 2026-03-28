f20c36b409eb085c5d8ad2d2e06e7c35463e948f
FRC5464 <bluejacketprogramming@gmail.com>
2026-03-28 13:51:39 -0500

    new belt motor

--- src/main/java/frc/robot/subsystems/BeltSubsystem.java
+++ src/main/java/frc/robot/subsystems/BeltSubsystem.java
@@ -5,12 +5,14 @@ import com.revrobotics.spark.SparkMax;
 // import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 // import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class BeltSubsystem extends SubsystemBase{
     
     private final SparkMax belt = new SparkMax(13, MotorType.kBrushless);
+    private final SparkMax agitator = new SparkMax(67,MotorType.kBrushless);
     
     public void periodicrunbelt(){
         // runBelt();
@@ -18,9 +20,11 @@ public class BeltSubsystem extends SubsystemBase{
     
     public void runBelt(double beltspd){
         belt.set(beltspd);
+        agitator.set(beltspd);
     }
 
     public void stopBelt(){
         belt.set(0);
+        agitator.set(0);
     }
 }
