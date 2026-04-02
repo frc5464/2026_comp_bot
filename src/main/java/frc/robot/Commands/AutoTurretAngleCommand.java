8dcff293674c44cf93f97b667f14f5936831a979
FRC5464 <94130236+frc5464@users.noreply.github.com>
2026-04-02 14:43:44 -0500

    if statement in case of empty photon results

--- src/main/java/frc/robot/Commands/AutoTurretAngleCommand.java
+++ src/main/java/frc/robot/Commands/AutoTurretAngleCommand.java
@@ -1,35 +1,41 @@
 package frc.robot.Commands;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 // import frc.robot.Constants.Vision;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.TurretSubsystem;
-import frc.robot.subsystems.Vision;
-import frc.robot.Robot;
+
 public class AutoTurretAngleCommand extends Command {
-    
+
     private CommandSwerveDrivetrain drivetrain;
     private TurretSubsystem turret;
 
     double[] dashboardVision;
 
-    public AutoTurretAngleCommand(CommandSwerveDrivetrain drivetrain, TurretSubsystem turret){
+    public AutoTurretAngleCommand(CommandSwerveDrivetrain drivetrain, TurretSubsystem turret) {
         this.drivetrain = drivetrain;
         this.turret = turret;
         addRequirements(turret);
     }
 
-        @Override
-        public void execute(){
-            dashboardVision = SmartDashboard.getNumberArray("robopose", new double[]{0,0,0});
+    @Override
+    public void execute() {
+        dashboardVision = SmartDashboard.getNumberArray("robopose", new double[] { 0, 0, 0 });
+        if (dashboardVision == new double[] { 0, 0, 0 }) {
+            turret.autoAim(drivetrain.getState().Pose.getX(), drivetrain.getState().Pose.getY(),
+                    drivetrain.getState().Pose.getRotation().getDegrees());
+        } else {
             turret.autoAim(
-                dashboardVision[0], 
-                dashboardVision[1],
-                dashboardVision[3]);
-            
-            /*turret.autoAim(drivetrain.getState().Pose.getX(),
-                           drivetrain.getState().Pose.getY(), 
-                           drivetrain.getState().Pose.getRotation().getDegrees());*/
+                    dashboardVision[0],
+                    dashboardVision[1],
+                    dashboardVision[3]);
         }
+        /*
+         * turret.autoAim(drivetrain.getState().Pose.getX(),
+         * drivetrain.getState().Pose.getY(),
+         * drivetrain.getState().Pose.getRotation().getDegrees());
+         */
+    }
 }
